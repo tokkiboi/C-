902dcf077f3287200f20c596381d9e0ca8d0b027
Alex Kim <alexkim80@gmail.com>
2015-12-07 02:22:02 -0800

    update

--- avggrade.cpp
+++ avggrade.cpp
@@ -8,41 +8,59 @@ Compute and output the average score. Together then we will add output the pin n
 #include <iomanip>
 #include <string>
 
+void getData(
 using namespace std;
 
 int main()
 {
 //Declare variables
-string name;
-int pin;
-double score=0;
-double avg=0; 
-double sum=0;
+//string names;
+string names[10];
+int pin[10];
+int score=[10];
+double avg=[0]; 
+double sum=[0];
 int count;
-string tempstr;
+int i;
+char ch='\n';
 
 //Input each number and add it into the sum
 
 
 //Calculate average
-avg = sum/static_cast<double>(count);
+//avg = sum/static_cast<double>(count);
 
 //Output the average
 ifstream myFile;
 myFile.open ("gradesWithNames.txt");
 if (myFile.fail()){cerr << "File is not found\n";}
 else {};
-int num=0;
+//int num=0;
+for (i=0; i <count;i++){
+//while (getline(myFile,names[10])){
 while (!myFile.eof()){
-getline (myFile,name);
-myFile >> pin >> score;
-getline (myFile,tempstr);
-sum=sum+score;
-num++;
+getline (myFile,names[i]);
+getData(pin[],i]);
+myFile >> score[i];
+myFile.get(ch);
+//getline (myFile,tempstr);
+sum+=score;
+avg = sum/(i+1);
+cout << "\033[0nName:\t" << names << endl;
+cout << "\033[1;32mPIN:\t" << pin << "\tSCORE:\t" << score << endl;
+cout << "\033[1;33mSUM:\t" << sum << endl;
+cout << "\033[1;33mAVG:\t" << avg << "\033[0m" << endl;
+count++;
 }
-
-cout << pin << endl;
-cout << sum << endl;
-cout << avg << endl;
+myFile.close();
+}
 return 0;
 }
+
+void getData(pinnum[],num){
+for (i=0; i < num; i++){
+	myFile >> pinnum[i];
+return;
+}
+}
+
